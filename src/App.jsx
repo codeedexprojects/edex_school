18453428d8cb9afcff746384773c16a9315c98d3
praveenaa47 <pravipraveena722@gmail.com>
2025-04-22 10:32:06 +0530

    scroll  commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Curriculam from './pages/Curriculam'
+import Scroll from './components/Scroll'
 
 
 function App() {
 
   return (
     <>
+  <Scroll/>
+  
   <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
